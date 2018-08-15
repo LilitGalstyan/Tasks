5a57b8a361170c26fa3d121725da7f64b59cd142
Lilit <lilitgalstyan333@gmail.com>
2018-08-15 21:17:57 +0400

    Adding new files

---
+++ Map_And_Reduce/MapAndReduce.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map_And_Reduce
+{
+    static class MapAndReduce
+    {
+        public static Result Reduce<T, Result>(this IEnumerable<T> list, Func<T, Result, Result> func, Result value)
+        {
+            Result val = value;
+
+            foreach (T item in list)
+            {
+                val = func(item, val);
+            }
+
+            return val;
+        }
+
+        public static List<Result> Map<T, Result>(this List<T> l, Func<T, Result> func)
+        {
+            return l.Reduce((x, list) => { list.Add(func(x)); return list; }, new List<Result>());
+        }
+    }
+}
